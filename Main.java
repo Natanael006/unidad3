ccaa0af04d1cbe494695aed8bf1643a5a5ef1dac
Natanael006 <143622953+Natanael006@users.noreply.github.com>
2023-09-26 19:22:49 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+
+
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+     Scanner ent = new Scanner (System.in);
+	 
+	 
+	     System.out.println("introduce el numero");
+	     N = ent.nexInt () ;
+	 
+	 int N;    
+	 if(N % 2 == 0 ){
+	 System.out.println("es par");
+	 }
+	 else{
+	     System.out.println("Es Impar");
+	 }
+	}
+}
