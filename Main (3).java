24fa51d73e27412263868f3f1cce375d7de919a2
Natanael006 <143622953+Natanael006@users.noreply.github.com>
2023-10-11 20:21:13 -0600

    Add files via upload
    en la aplicación que lo hice no me corre entonces no pude poner la captura

---
+++ Main (3).java
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+
+public class Seriefibonacci{
+{
+	public static void main(String[] args) {
+		
+		
+	Scanner teclado = new Scanner(System.in);
+	
+	int a = 0, b = 1, d, n;
+	
+	System.out.print("ingresa los datos para la serie");
+	n =  teclado.nextInt();
+	
+	for (int i = 0; i < n; i++) {
+	    System.out.println(a);
+	    d = a+b;
+	    a=b
+	    b=d
+	}
+	}
+}
+}
